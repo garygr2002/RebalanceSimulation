e57df2e50964d588bde8baa8bb9b317885b762d0
Gary Gregg <garygr2002@yahoo.com>
2022-08-29 16:50:16 -0700

    Added 'zero' as a heading column in the report.

--- RebalanceSimulation.R
+++ RebalanceSimulation.R
@@ -10,7 +10,7 @@ library(webshot)
 # ending value is too high.
 #
 if (!(exists('raw') && is.data.frame(get('raw')))) {
-  raw <- read.csv("data/rebalance_simulation1.csv", header=TRUE)
+  raw <- read.csv("data/rebalance_simulation.csv", header=TRUE)
 }
 #
 # Place limits on upside limits on the high, and bear market equity
@@ -18,20 +18,23 @@ if (!(exists('raw') && is.data.frame(get('raw')))) {
 # about equal its inflation-adjusted start value of $1,000,000.00. Sort the
 # remaining rows first by descending draw, second by descending value.
 # 
-exclude <- subset(raw, (high <= 0.50) & (bear <= 0.58) &
-                    (value <= 10000000.00))
-sorted <- exclude[order(-exclude$draw, -exclude$value),]
+# exclude <- subset(raw, (high <= 0.50) & (bear <= 0.58) &
+#                     (value <= 10000000.00))
+exclude <- raw
+sorted <- exclude[order(exclude$draw, -exclude$value),]
 #
 # Get the first one hundred rows, and add a sequence column. Put the sequence
 # column first.
 #
 firstRows <- head(sorted, 100)
+sapply(firstRows, FUN=mean)
 firstRows$row <- seq.int(nrow(firstRows))
 firstRows <-firstRows[, c("row",
                           "discretionary",
                           "fixed",
                           "high",
                           "bear",
+                          "zero",
                           "advance",
                           "decline",
                           "value",
@@ -54,7 +57,10 @@ table <-firstRows %>%
     columns = c(draw, value)
   ) %>%
   fmt_percent(
-    columns = c(discretionary, fixed, high, bear, advance, decline)
+    columns = c(discretionary, fixed),
+    decimals = 3
+  ) %>%
+  fmt_percent(
+    columns = c(high, bear, zero, advance, decline),
   )
 table
-# gtsave(table, "rebalance_simulation.pdf", zoom = 1)
